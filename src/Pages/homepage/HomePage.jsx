70503ef696a9d6afda7de783b8eba750db655c23
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-15 01:14:48 +0600

    commit

--- src/Pages/homepage/HomePage.jsx
+++ src/Pages/homepage/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h2>Homepage</h2>
+      <h2>Homepag</h2>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, } from 'react-router'
 import { RouterProvider } from 'react-router/dom';
-
+import MainLayout from './Layout/MainLayout';
+import Books from './Pages/Books/Books';
+import HomePage from './Pages/homepage/HomePage';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: "Homepage",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        element: <HomePage></HomePage>
+      },
+      {
+        path: "/books",
+        element: <Books></Books>
+      }
+    ]
   },
- 
-  
+
 ]);
 
 createRoot(document.getElementById('root')).render(
