f5a7dc887bd238ac8cebac24bd3a033b4fd8b05d
rezuyanahmmed <rafiahmmed457@gmail.com>
2026-05-17 20:13:18 +0600

    commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -9,10 +9,7 @@ export const router = createBrowserRouter([
     path: "/",
     element: <MainLayout></MainLayout>,
     children: [
-      {
-        index: true,
-        element: <HomePage></HomePage>
-      },
+      
       {
         path: "/books",
         element: <Books></Books>
